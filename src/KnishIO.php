295f423d63aed76b4f961b2a4a9e706b041ba65e
Zaur <len1in@yandex.ru>
2019-09-06 14:30:03 +0300

    Now it’s possible to get balance on the bundle

--- src/KnishIO.php
+++ src/KnishIO.php
@@ -19,7 +19,7 @@ class KnishIO
 	private static $url = 'https://wishknish.com/graphql';
 	private static $client;
 	private static $query = [
-		'molecule' => 'mutation MoleculeMutation( $molecule: MoleculeInput! ) { ProposeMolecule( molecule: $molecule, ) { molecularHash, height, depth, status, reason, reasonPayload, createdAt, receivedAt, processedAt, broadcastedAt } }',
+		'molecule' => 'mutation( $molecule: MoleculeInput! ) { ProposeMolecule( molecule: $molecule, ) { molecularHash, height, depth, status, reason, reasonPayload, createdAt, receivedAt, processedAt, broadcastedAt } }',
 		'balance'  => 'query( $address: String, $bundleHash: String, $token: String, $position: String ) { Balance( address: $address, bundleHash: $bundleHash, token: $token, position: $position ) { address, bundleHash, tokenSlug, position, amount, createdAt } }',
 	];
 
@@ -40,34 +40,35 @@ class KnishIO
 		static::$client = null;
 	}
 
-	/**
-	 * @param string $secret
-	 * @param string $token
-	 * @return Wallet|null
-	 * @throws \Exception
-	 */
-	public static function getBalance ( $secret, $token )
-	{
-		$wallet = null;
-		$response = static::request(
-			static::$query[ 'balance' ],
-			[ 'bundleHash' => Crypto::generateBundleHash( $secret ), 'token' => $token ]
-		);
+    /**
+     * @param string $code
+     * @param string $token
+     * @return Wallet|null
+     * @throws \Exception|\ReflectionException|InvalidResponseException
+     */
+    public static function getBalance ( $code, $token )
+    {
+        $wallet = null;
+        $response = static::request(
+            static::$query[ 'balance' ],
+            [
+                'bundleHash' => mb_strlen( ( string ) $code ) === 64 ? $code : Crypto::generateBundleHash( $code ),
+                'token' => $token,
+            ]
+        );
 
-		if ( isset( $response[ 'data' ][ 'Balance' ] ) ) {
+        if ( isset( $response[ 'data' ] ) && isset( $response[ 'data' ][ 'Balance' ] ) ) {
+            $balance = $response[ 'data' ][ 'Balance' ];
+            $wallet = new Wallet( $code, $balance[ 'tokenSlug' ] );
+            $wallet->address = $balance[ 'address' ];
+            $wallet->position = $balance[ 'position' ];
+            $wallet->balance = $balance[ 'amount' ];
+            $wallet->bundle = $balance[ 'bundleHash' ];
+            unset( $wallet->key );
+        }
 
-			$balance = $response[ 'data' ][ 'Balance' ];
-			$wallet = new Wallet( $secret, $balance[ 'tokenSlug' ], $balance[ 'position' ] );
-
-			if ( $wallet->address !== $balance[ 'address' ] ) {
-				$wallet->address = $balance[ 'address' ];
-			}
-
-			$wallet->balance = $balance[ 'amount' ];
-		}
-
-		return $wallet;
-	}
+        return $wallet;
+    }
 
 	/**
 	 * @param string $secret
@@ -75,7 +76,7 @@ class KnishIO
 	 * @param int|float $amount
 	 * @param array $metas
 	 * @return array
-	 * @throws \Exception|\ReflectionException
+	 * @throws \Exception|\ReflectionException|InvalidResponseException
 	 */
 	public static function createToken ( $secret, $token, $amount, array $metas = [] )
 	{
@@ -100,7 +101,7 @@ class KnishIO
 	 * @param string $token
 	 * @param int|float $amount
 	 * @return array
-	 * @throws \Exception|\ReflectionException
+	 * @throws \Exception|\ReflectionException|InvalidResponseException
 	 */
 	public static function transferToken ( $fromSecret, $toSecret, $token, $amount )
 	{
@@ -147,6 +148,7 @@ class KnishIO
 	 * @param string $query
 	 * @param array $variables
 	 * @return array
+     * @throws InvalidResponseException
 	 */
 	private static function request ( $query, $variables )
 	{
