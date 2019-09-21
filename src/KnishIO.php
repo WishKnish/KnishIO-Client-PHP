fc28f43781aabe3d94383fd61762631e8a73475c
Eugene Teplitsky <eugene@wishknish.com>
2019-09-21 15:35:20 -0400

    Removing redundant Molecule::verify call from init methods

--- src/KnishIO.php
+++ src/KnishIO.php
@@ -40,39 +40,39 @@ class KnishIO
 		static::$client = null;
 	}
 
-    /**
-     * @param string $code
-     * @param string $token
-     * @return Wallet|null
-     * @throws \Exception|\ReflectionException|InvalidResponseException
-     */
-    public static function getBalance ( $code, $token )
-    {
-        $wallet = null;
-        $response = static::request(
-            static::$query[ 'balance' ],
-            [
-                // If bundle hash came, we pass it, otherwise we consider it a secret
-                'bundleHash' => mb_strlen( ( string ) $code ) === 64 ? $code : Crypto::generateBundleHash( $code ),
-                'token' => $token,
-            ]
-        );
+	/**
+	 * @param string $code
+	 * @param string $token
+	 * @return Wallet|null
+	 * @throws \Exception|\ReflectionException|InvalidResponseException
+	 */
+	public static function getBalance ( $code, $token )
+	{
+		$wallet = null;
+		$response = static::request(
+			static::$query[ 'balance' ],
+			[
+				// If bundle hash came, we pass it, otherwise we consider it a secret
+				'bundleHash' => mb_strlen( ( string ) $code ) === 64 ? $code : Crypto::generateBundleHash( $code ),
+				'token'      => $token,
+			]
+		);
 
-        if ( isset( $response[ 'data' ] ) && isset( $response[ 'data' ][ 'Balance' ] ) ) {
-            $balance = $response[ 'data' ][ 'Balance' ];
+		if ( isset( $response[ 'data' ] ) && isset( $response[ 'data' ][ 'Balance' ] ) ) {
+			$balance = $response[ 'data' ][ 'Balance' ];
 
-            if ( $balance[ 'tokenSlug' ] === $token ) {
-                $wallet = new Wallet( $code, $balance[ 'tokenSlug' ] );
-                $wallet->address = $balance[ 'address' ];
-                $wallet->position = $balance[ 'position' ];
-                $wallet->balance = $balance[ 'amount' ];
-                $wallet->bundle = $balance[ 'bundleHash' ];
-                unset( $wallet->key );
-            }
-        }
+			if ( $balance[ 'tokenSlug' ] === $token ) {
+				$wallet = new Wallet( $code, $balance[ 'tokenSlug' ] );
+				$wallet->address = $balance[ 'address' ];
+				$wallet->position = $balance[ 'position' ];
+				$wallet->balance = $balance[ 'amount' ];
+				$wallet->bundle = $balance[ 'bundleHash' ];
+				unset( $wallet->key );
+			}
+		}
 
-        return $wallet;
-    }
+		return $wallet;
+	}
 
 	/**
 	 * @param string $secret
@@ -88,42 +88,35 @@ class KnishIO
 		$molecule->initTokenCreation( new Wallet( $secret ), new Wallet( $secret, $token ), $amount, $metas );
 		$molecule->sign( $secret );
 
-        if ( ! Molecule::verifyTokenIsotopeV( $molecule ) ) {
-            return [
-                'status' => 'rejected',
-                'reason' => 'Incorrect "V" isotopes in a molecule',
-            ];
-        }
-
-        if ( ! Molecule::verifyMolecularHash( $molecule ) ) {
-            return [
-                'status' => 'rejected',
-                'reason' => 'Wrong hash for the molecule',
-            ];
-        }
-
-        if ( ! Molecule::verifyOts( $molecule ) ) {
-            return [
-                'status' => 'rejected',
-                'reason' => 'Wrong OTS in the molecule',
-            ];
-        }
-
-		if ( Molecule::verify( $molecule ) ) {
-			$response = static::request( static::$query[ 'molecule' ], [ 'molecule' => $molecule, ] );
-			return \array_intersect_key(
-				$response[ 'data' ][ 'ProposeMolecule' ] ?: [
-				    'status' => 'rejected',
-                    'reason' => 'Invalid response from server',
-                ],
-				\array_flip( [ 'reason', 'status', ] )
-			);
+		if ( !Molecule::verifyTokenIsotopeV( $molecule ) ) {
+			return [
+				'status' => 'rejected',
+				'reason' => 'Incorrect "V" isotopes in a molecule',
+			];
 		}
 
-		return [
-		    'status' => 'rejected',
-            'reason' => 'The address of the molecule is not correctly generated',
-        ];
+		if ( !Molecule::verifyMolecularHash( $molecule ) ) {
+			return [
+				'status' => 'rejected',
+				'reason' => 'Wrong hash for the molecule',
+			];
+		}
+
+		if ( !Molecule::verifyOts( $molecule ) ) {
+			return [
+				'status' => 'rejected',
+				'reason' => 'Wrong OTS in the molecule',
+			];
+		}
+
+		$response = static::request( static::$query[ 'molecule' ], [ 'molecule' => $molecule, ] );
+		return \array_intersect_key(
+			$response[ 'data' ][ 'ProposeMolecule' ] ?: [
+				'status' => 'rejected',
+				'reason' => 'Invalid response from server',
+			],
+			\array_flip( [ 'reason', 'status', ] )
+		);
 	}
 
 	/**
@@ -137,66 +130,58 @@ class KnishIO
 	public static function transferToken ( $fromSecret, $to, $token, $amount )
 	{
 		$fromWallet = static::getBalance( $fromSecret, $token );
-
-		if ( null === $fromWallet || $amount > $fromWallet->balance ) {
+		if ( $fromWallet === null || $fromWallet->balance < $amount ) {
 			return [
-			    'status' => 'rejected',
-                'reason' => 'The transfer amount cannot be greater than the sender\'s balance',
-            ];
+				'status' => 'rejected',
+				'reason' => 'The transfer amount cannot be greater than the sender\'s balance',
+			];
 		}
 
 		// If this wallet is assigned, if not, try to get a valid wallet
-        $toWallet = $to instanceof Wallet ? $to : static::getBalance( $to, $token );
+		$toWallet = $to instanceof Wallet ? $to : static::getBalance( $to, $token );
 
 		// If the wallet is not transferred in a variable and the user does not have a balance wallet,
-        // then create a new one for him
-		if ( null === $toWallet ) {
-            $toWallet = new Wallet( $to, $token );
-        }
+		// then create a new one for him
+		if ( $toWallet === null ) {
+			$toWallet = new Wallet( $to, $token );
+		}
 
 		$molecule = new Molecule();
 		$molecule->initValue( $fromWallet, $toWallet, new Wallet( $fromSecret, $token ), $amount );
 		$molecule->sign( $fromSecret );
 
-		if ( ! Molecule::verifyTokenIsotopeV( $molecule ) ) {
-            return [
-                'status' => 'rejected',
-                'reason' => 'Incorrect "V" isotopes in a molecule',
-            ];
-        }
-
-		if ( ! Molecule::verifyMolecularHash( $molecule ) ) {
-            return [
-                'status' => 'rejected',
-                'reason' => 'Wrong hash for the molecule',
-            ];
-        }
-
-		if ( ! Molecule::verifyOts( $molecule ) ) {
-            return [
-                'status' => 'rejected',
-                'reason' => 'Wrong OTS in the molecule',
-            ];
-        }
-
-		if ( Molecule::verify( $molecule ) ) {
-			$response = static::request( static::$query[ 'molecule' ], [ 'molecule' => $molecule, ] );
-			return \array_intersect_key(
-				$response[ 'data' ][ 'ProposeMolecule' ] ?: [
-				    'status' => 'rejected',
-                    'reason' => 'Invalid response from server',
-                ],
-				\array_flip( [
-				    'reason',
-                    'status',
-                ] )
-			);
+		if ( !Molecule::verifyTokenIsotopeV( $molecule ) ) {
+			return [
+				'status' => 'rejected',
+				'reason' => 'Incorrect "V" isotopes in a molecule',
+			];
 		}
 
-		return [
-		    'status' => 'rejected',
-            'reason' => 'The address of the molecule is not correctly generated',
-        ];
+		if ( !Molecule::verifyMolecularHash( $molecule ) ) {
+			return [
+				'status' => 'rejected',
+				'reason' => 'Wrong hash for the molecule',
+			];
+		}
+
+		if ( !Molecule::verifyOts( $molecule ) ) {
+			return [
+				'status' => 'rejected',
+				'reason' => 'Wrong OTS in the molecule',
+			];
+		}
+
+		$response = static::request( static::$query[ 'molecule' ], [ 'molecule' => $molecule, ] );
+		return \array_intersect_key(
+			$response[ 'data' ][ 'ProposeMolecule' ] ?: [
+				'status' => 'rejected',
+				'reason' => 'Invalid response from server',
+			],
+			\array_flip( [
+				'reason',
+				'status',
+			] )
+		);
 	}
 
 	/**
@@ -210,9 +195,9 @@ class KnishIO
 				'verify'      => false,
 				'http_errors' => false,
 				'headers'     => [
-				    'User-Agent' => 'KnishIO/0.1',
-                    'Accept' => 'application/json',
-                ]
+					'User-Agent' => 'KnishIO/0.1',
+					'Accept'     => 'application/json',
+				]
 			] );
 		}
 
@@ -223,19 +208,19 @@ class KnishIO
 	 * @param string $query
 	 * @param array $variables
 	 * @return array
-     * @throws InvalidResponseException
+	 * @throws InvalidResponseException
 	 */
 	private static function request ( $query, $variables )
 	{
 		$response = static::getClient()->post( null, [
-		    'json' => [
-		        'query' => $query,
-                'variables' => $variables,
-            ]
-        ] );
+			'json' => [
+				'query'     => $query,
+				'variables' => $variables,
+			]
+		] );
 		$responseJson = \json_decode( $response->getBody()->getContents(), true );
 
-		if ( null === $responseJson ) {
+		if ( $responseJson === null ) {
 			throw new InvalidResponseException();
 		}
 
