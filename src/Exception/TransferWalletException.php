9c136057651b9df91823f2ea304ab475adf2aa34
Eugene Teplitsky <eugene@wishknish.com>
2019-10-28 15:32:13 -0400

    Adding new exception for missing sender wallet to differentiate from remainder exception

---
+++ src/Exception/TransferWalletException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WishKnish\KnishIO\Client\Exception;
+
+/**
+ * Class TransferWalletException
+ * @package WishKnish\KnishIO\Client\Exception
+ *
+ * @property string $message
+ * @property integer $code
+ * @property string $file
+ * @property integer $line
+ */
+class TransferWalletException extends BaseException
+{
+	/**
+	 * TransferRemainderException constructor.
+	 * @param string $message
+	 * @param int $code
+	 * @param \Throwable|null $previous
+	 */
+	public function __construct ( $message = 'Sender wallet is missing or invalid', $code = 1, \Throwable $previous = null )
+	{
+		parent::__construct( $message, $code, $previous );
+	}
+}
