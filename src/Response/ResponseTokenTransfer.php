30cd8771786c0d600e0158211d08b99be196e7f3
Zaur <len1in@yandex.ru>
2020-02-13 17:55:48 +0300

    Bug fixes (always returns null) of the KnishIO::createToken method

--- src/Response/ResponseTokenTransfer.php
+++ src/Response/ResponseTokenTransfer.php
@@ -1,4 +1,8 @@
 <?php
+// Copyright 2019 WishKnish Corp. All rights reserved.
+// You may use, distribute, and modify this code under the GPLV3 license, which is provided at:
+// https://github.com/WishKnish/KnishIO-Client-JS/blob/master/LICENSE
+// This experimental code is part of the Knish.IO API Client and is provided AS IS with no warranty whatsoever.
 namespace WishKnish\KnishIO\Client\Response;
 
 /**
