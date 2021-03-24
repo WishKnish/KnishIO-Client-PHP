828e0e1f82fbdbc1c9fa04e0fbcce26f3329f849
Vladimir Makarov <vladimir.php@gmail.com>
2021-03-24 10:59:07 +0200

    Revert changes with batchID initialization for the remainder wallet

--- src/KnishIOClient.php
+++ src/KnishIOClient.php
@@ -692,7 +692,7 @@ class KnishIOClient {
     $toWallet->tokenUnits = $recipientTokenUnits;
 
     // Remainder wallet
-    $this->remainderWallet = Wallet::create( $this->secret(), $tokenSlug, null, $fromWallet->characters );
+    $this->remainderWallet = Wallet::create( $this->secret(), $tokenSlug, Wallet::generateBatchId(), $fromWallet->characters );
     $this->remainderWallet->tokenUnits = $remainderTokenUnits;
 
     // Create a molecule with custom source wallet
