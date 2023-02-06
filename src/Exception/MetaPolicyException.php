c9f43c75955cdb2a85cdc7c7e36c649c85916b7f
Eugene Teplitsky <eugene@wishknish.com>
2023-02-06 00:12:01 -0500

    Adding MetaPolicyException

---
+++ src/Exception/MetaPolicyException.php
@@ -0,0 +1,70 @@
+<?php
+/*
+                               (
+                              (/(
+                              (//(
+                              (///(
+                             (/////(
+                             (//////(                          )
+                            (////////(                        (/)
+                            (////////(                       (///)
+                           (//////////(                      (////)
+                           (//////////(                     (//////)
+                          (////////////(                    (///////)
+                         (/////////////(                   (/////////)
+                        (//////////////(                  (///////////)
+                        (///////////////(                (/////////////)
+                       (////////////////(               (//////////////)
+                      (((((((((((((((((((              (((((((((((((((
+                     (((((((((((((((((((              ((((((((((((((
+                     (((((((((((((((((((            ((((((((((((((
+                    ((((((((((((((((((((           (((((((((((((
+                    ((((((((((((((((((((          ((((((((((((
+                    (((((((((((((((((((         ((((((((((((
+                    (((((((((((((((((((        ((((((((((
+                    ((((((((((((((((((/      (((((((((
+                    ((((((((((((((((((     ((((((((
+                    (((((((((((((((((    (((((((
+                   ((((((((((((((((((  (((((
+                   #################  ##
+                   ################  #
+                  ################# ##
+                 %################  ###
+                 ###############(   ####
+                ###############      ####
+               ###############       ######
+              %#############(        (#######
+             %#############           #########
+            ############(              ##########
+           ###########                  #############
+          #########                      ##############
+        %######
+
+        Powered by Knish.IO: Connecting a Decentralized World
+
+Please visit https://github.com/WishKnish/KnishIO-Client-PHP for information.
+
+License: https://github.com/WishKnish/KnishIO-Client-PHP/blob/master/LICENSE
+ */
+
+namespace WishKnish\KnishIO\Client\Exception;
+
+use Throwable;
+
+/**
+ * Class MetaPolicyException
+ * @package WishKnish\KnishIO\Client\Exception
+ */
+class MetaPolicyException extends KnishIOException {
+    /**
+     * UnauthenticatedException constructor.
+     *
+     * @param string $message
+     * @param $payload
+     * @param int $code
+     * @param Throwable|null $previous
+     */
+    public function __construct ( string $message = 'Attempting to violate meta policy.', $payload = null, int $code = 2, Throwable $previous = null ) {
+        parent::__construct( $message, $payload, $code, $previous );
+    }
+}
