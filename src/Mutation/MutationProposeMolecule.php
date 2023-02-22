0d1fd850b676b315a140658ddacdadb0a291b82f
Eugene Teplitsky <eugene@wishknish.com>
2023-02-22 10:56:00 -0500

    Refactoring out MutationProposeMoleculeStructure

--- src/Mutation/MutationProposeMolecule.php
+++ src/Mutation/MutationProposeMolecule.php
@@ -49,15 +49,43 @@ License: https://github.com/WishKnish/KnishIO-Client-PHP/blob/master/LICENSE
 
 namespace WishKnish\KnishIO\Client\Mutation;
 
+use JetBrains\PhpStorm\Pure;
+use WishKnish\KnishIO\Client\Exception\KnishIOException;
 use WishKnish\KnishIO\Client\HttpClient\HttpClientInterface;
 use WishKnish\KnishIO\Client\Molecule;
+use WishKnish\KnishIO\Client\Query\Query;
+use WishKnish\KnishIO\Client\Response\ResponseMolecule;
 use WishKnish\KnishIO\Client\Wallet;
 
 /**
  * Class MutationProposeMolecule
  * @package WishKnish\KnishIO\Client\Mutation
  */
-class MutationProposeMolecule extends MutationProposeMoleculeStructure {
+class MutationProposeMolecule extends Query {
+
+    // Query
+    protected static string $defaultQuery = 'mutation( $molecule: MoleculeInput! ) { ProposeMolecule( molecule: $molecule )
+		@fields
+	}';
+
+    // Fields
+    protected array $fields = [
+        'molecularHash',
+        'height',
+        'depth',
+        'status',
+        'reason',
+        'payload',
+        'createdAt',
+        'receivedAt',
+        'processedAt',
+        'broadcastedAt',
+    ];
+
+    /**
+     * @var bool
+     */
+    protected bool $isMutation = true;
 
     /**
      * @var Molecule
@@ -75,10 +103,12 @@ class MutationProposeMolecule extends MutationProposeMoleculeStructure {
      * @param HttpClientInterface $client
      * @param Molecule $molecule
      * @param string|null $query
+     *
      */
     public function __construct ( HttpClientInterface $client, Molecule $molecule, string $query = null ) {
-        parent::__construct( $client, $molecule, $query );
+        parent::__construct( $client, $query );
 
+        // Create a molecule
         $this->molecule = $molecule;
     }
 
@@ -94,6 +124,31 @@ class MutationProposeMolecule extends MutationProposeMoleculeStructure {
      */
     public function remainderWallet (): Wallet {
         return $this->remainderWallet;
+    }
+
+
+    /**
+     * @param array $variables
+     *
+     * @return array
+     */
+    #[Pure]
+    public function compiledVariables ( array $variables ): array {
+        // Default variables
+        $variables = parent::compiledVariables( $variables );
+
+        // Merge variables with a molecule key
+        return array_merge( $variables, [ 'molecule' => $this->molecule ] );
+    }
+
+    /**
+     * @param string $response
+     *
+     * @return ResponseMolecule
+     * @throws KnishIOException
+     */
+    public function createResponse ( string $response ): ResponseMolecule {
+        return new ResponseMolecule( $this, $response );
     }
 
 }
